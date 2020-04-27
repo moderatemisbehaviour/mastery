29aff202a8545e4f29507620aa2adb723b3fa46f
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2020-04-27 13:38:32 +0100

    Fix expected Cypress config path

--- update-cypress-dashboard-api.sh
+++ update-cypress-dashboard-api.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 VERSION=4.4.1
-CYPRESS_CONFIG_FILE_PATH=$(npx cypress cache path)/$VERSION/Cypress.app/Contents/Resources/app/packages/server/config/app.yml
+CYPRESS_CONFIG_FILE_PATH=$(npx cypress cache path)/$VERSION/Cypress/resources/app/packages/server/config/app.yml 
 sed -i 's/https:\/\/api.cypress.io\//http:\/\/localhost:1234\//g' $CYPRESS_CONFIG_FILE_PATH
