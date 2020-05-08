5a0c6dc980fae6cae73c7479233a4caa574f9bc7
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2020-05-08 21:18:12 +0100

    Update script to handle changing Cypress version

--- ci/sorry-cypress/update-cypress-dashboard-api-url.sh
+++ ci/sorry-cypress/update-cypress-dashboard-api-url.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-VERSION=4.4.1
-CYPRESS_CONFIG_FILE_PATH=$(npx cypress cache path)/$VERSION/Cypress/resources/app/packages/server/config/app.yml 
+VERSION=$(yarn list --pattern cypress | grep cypress@ | sed 's/.*cypress@//g')
+CYPRESS_CONFIG_FILE_PATH=$(npx cypress cache path)/$yarn/Cypress/resources/app/packages/server/config/app.yml 
 sed -i 's/https:\/\/api.cypress.io\//https:\/\/sorry-cypress-dot-moderatemisbehaviour.nw.r.appspot.com/g' $CYPRESS_CONFIG_FILE_PATH
