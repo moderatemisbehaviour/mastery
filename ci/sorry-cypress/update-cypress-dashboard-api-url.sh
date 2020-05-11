659ddb3097f8d246babc718e6e29bb26bea6fe65
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2020-05-11 11:49:58 +0100

    Update Cypress version

--- ci/sorry-cypress/update-cypress-dashboard-api-url.sh
+++ ci/sorry-cypress/update-cypress-dashboard-api-url.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-VERSION=$(yarn list --pattern cypress | grep cypress@ | sed 's/.*cypress@//g')
+VERSION=4.5.0
 CYPRESS_CONFIG_FILE_PATH=$(npx cypress cache path)/$VERSION/Cypress/resources/app/packages/server/config/app.yml
 sed -i 's/https:\/\/api.cypress.io\//https:\/\/sorry-cypress-dot-moderatemisbehaviour.nw.r.appspot.com/g' $CYPRESS_CONFIG_FILE_PATH
